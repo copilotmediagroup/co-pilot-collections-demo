ce0c6cbf22a40e94d51f7bc94978f9fd3829190d
copilotmediagroup <copilotmediagroup@gmail.com>
2026-06-18 01:21:47 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 // Co Pilot Collections Manager - DEMO Supabase Config
-// This file uses the Supabase publishable key only.
-// Do NOT put your service_role / secret key here.
-
 window.CO_PILOT_SUPABASE_CONFIG = {
   url: "https://nixqpmgkmrzjsaqhqqew.supabase.co",
   publishableKey: "sb_publishable_g4D2rVDfqyGJLe1yhShBjQ_t6Oobilb"
